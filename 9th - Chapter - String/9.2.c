5396abce730b84c1fd06903f46d1e3be15e9fdc4
Saad Bin Manjur Adit <49765532+saadbinmanjur@users.noreply.github.com>
2020-08-21 20:17:41 +0600

    Add files via upload

---
+++ 9th - Chapter - String/9.2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    char country[]={'B','a','n','g','l','a','d','e','s','h',' ','i','s',' ','m','y',' ','c','o','u','n','t','r','y'};
+    printf("\n\n%s\n\n",country);
+    return 0;
+}
+
