250573b37fba2e4b7dbddea49f8a847429b5f55a
Saad Bin Manjur Adit <49765532+saadbinmanjur@users.noreply.github.com>
2020-08-29 01:34:25 +0600

    Add files via upload

---
+++ 9th - Chapter - String/9.6.2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int string_length(char str[])
+{
+    int i=0;
+    while(str[i]!='\0')
+    {
+        i++;
+    }
+    return i;
+}
+int main()
+{
+    char country[100];
+    int length;
+    printf("Enter your words: ");
+    while(1==scanf("%s",country))
+    {
+        length=string_length(country);
+        printf("\n\nLength: %d\n\n",length);
+    }
+    return 0;
+}
+
+
+
