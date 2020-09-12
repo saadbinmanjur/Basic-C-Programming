66989085e79bafd2e58445b732c637c6f857d34f
Saad Bin Manjur Adit <49765532+saadbinmanjur@users.noreply.github.com>
2020-09-12 12:42:34 +0600

    Add files via upload

---
+++ 11th - Chapter - Array Again/11.3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char saarc[7][100]={"Bangladesh",
+    "India",
+    "Pakistan",
+    "Sri Lanka",
+    "Nepal",
+    "Bhutan",
+    "Maldives"};
+    int row;
+    for(row=0;row<7;row++)
+    {
+        printf("\n\n%s\n\n",saarc[row]);
+    }
+    return 0;
+}
